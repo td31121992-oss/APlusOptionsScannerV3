2de9b4eb1e7902b766ba6415d4eb5e8795b3f553
td31121992-oss <td31121992@gmail.com>
2026-09-24 23:36:33 +0530

    Export news collection and reporting interfaces

--- news_intelligence/__init__.py
+++ news_intelligence/__init__.py
@@ -3,6 +3,15 @@
 The package is intentionally provider-agnostic and paper-signal only.
 """
 
+from .collector import NewsCollector, StaticNewsCollector
 from .models import NewsEvent, NewsImpact, NewsSource
+from .report_writer import NewsReportWriter
 
-__all__ = ["NewsEvent", "NewsImpact", "NewsSource"]
+__all__ = [
+    "NewsCollector",
+    "NewsEvent",
+    "NewsImpact",
+    "NewsReportWriter",
+    "NewsSource",
+    "StaticNewsCollector",
+]
