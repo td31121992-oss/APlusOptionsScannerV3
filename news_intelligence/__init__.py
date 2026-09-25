5bf660450344bc2587709ce7625eb74ec5b4ac90
td31121992-oss <td31121992@gmail.com>
2026-09-25 09:20:01 +0530

    feat: export dashboard news bridge

--- news_intelligence/__init__.py
+++ news_intelligence/__init__.py
@@ -5,6 +5,7 @@ The package is intentionally provider-agnostic and paper-signal only.
 
 from .collector import NewsCollector, StaticNewsCollector
 from .market_watch_adapter import MarketWatchNewsAnnotation, annotate_market_watch
+from .market_watch_dashboard_bridge import enrich_dashboard_market_watch
 from .market_watch_payload import build_market_watch_payload
 from .market_watch_report_adapter import enrich_market_watch_report
 from .models import NewsEvent, NewsImpact, NewsSource
@@ -23,5 +24,6 @@ __all__ = [
     "StaticNewsCollector",
     "annotate_market_watch",
     "build_market_watch_payload",
+    "enrich_dashboard_market_watch",
     "enrich_market_watch_report",
 ]
