4f66ff9255050e425da8e00ff55e327f431ce568
td31121992-oss <td31121992@gmail.com>
2026-09-25 08:58:53 +0530

    feat: export market shock early warning engine

--- news_intelligence/__init__.py
+++ news_intelligence/__init__.py
@@ -6,8 +6,11 @@ The package is intentionally provider-agnostic and paper-signal only.
 from .collector import NewsCollector, StaticNewsCollector
 from .models import NewsEvent, NewsImpact, NewsSource
 from .report_writer import NewsReportWriter
+from .shock_engine import MarketShockEarlyWarning, MarketShockWarning
 
 __all__ = [
+    "MarketShockEarlyWarning",
+    "MarketShockWarning",
     "NewsCollector",
     "NewsEvent",
     "NewsImpact",
